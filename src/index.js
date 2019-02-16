b40c075e0752500c69045dd0598a8c909e05c7d3
韩前进 <qianjin.han@hand-china.com>
2019-02-17 01:24:52 +0800

    根据传参进行代理检测

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const router = require('./proxy/router')
 const { createServer } = require('http')
 const checkProxies = require('./proxy/checkProxies')
 const proxyController = require('./proxy/proxyController')
+const isNaN = require('./utils/isNaN')
 const app = createServer(router)
 const io = require('socket.io')(app)
 
@@ -11,8 +12,11 @@ io.on('connection', async function (socket) {
     data: '连接已建立',
     msg: '成功'
   })
-  await checkProxies({ socket, t: 1000, ...await proxyController(1) })
-  await checkProxies({ socket, t: 1000, ...await proxyController(2) })
+  socket.on('proxy', async data => {
+    const { mode = 'xici' , timeout = 1000 } = data
+    const t = isNaN(timeout) ? 1000 : Number.parseInt(timeout)
+    checkProxies({ socket, t, ...await proxyController(mode === 'xici' ? 1 : 2)})
+  })
 })
 
 app.listen(80, err => {
